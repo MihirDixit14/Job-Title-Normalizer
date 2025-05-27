262d90155c2219f85044eb299951884fc4a1258d
MihirDixit14 <mdixit@uwm.edu>
2025-05-27 01:36:38 -0600

    main.py updated

--- app/main.py
+++ app/main.py
@@ -7,5 +7,14 @@ app=FastAPI(title="Job Title Normalizer API")
 class TitleRequest(BaseModel):
     title:str
 
+class TitleResponse(BaseModel):
+    normalize_title:str
+@app.post("/normalization-title",response_model=TitleResponse)
+def normalize_title_endpoint(req: TitleRequest):
+    normalized=normalize_title(req.title)
+    return TitleResponse(normalize_title=normalized)
+
+
+
 
                                         
